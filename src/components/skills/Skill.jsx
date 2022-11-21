b1fb0620fa38b2d645cb908b40a73833bc6744a2
George Kutsia <Georgekutsia@gmail.com>
2022-11-21 18:42:09 +0100

    building

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 function Skill({image, name, link}) {
   return (
     <div>
+    
         <div className='skills-little-box mb-4'>
         <a href={link} ><img  className='image-skills' src={image} alt="Techno" /></a>
           <div className='text-skills'>{name}</div>
